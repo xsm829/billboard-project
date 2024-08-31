4ec7d609733f858297512a2771a7a77709819aa2
xsm829 <158262967+xsm829@users.noreply.github.com>
2024-08-31 18:43:54 +0800

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,5 @@
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `name`, `phone`, `address`, `isadmin`, `isvalidate`) VALUES (1, 'admin', 'admin@vilicode.com', 'admin', '管理员', '1333333333', '中华人民共和国', b'1', b'0');
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `name`, `phone`, `address`, `isadmin`, `isvalidate`) VALUES (2, 'vili', 'vili@vilicode.com', 'vili', 'vili', '1344444444', '中华人民共和国', b'0', b'0');
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `name`, `phone`, `address`, `isadmin`, `isvalidate`) VALUES (3, '1', '1@1', '1', '1', '1', '1', b'0', b'0');
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `name`, `phone`, `address`, `isadmin`, `isvalidate`) VALUES (38, '222', '123@456.com', '222', '222', '222', '123@456.com', b'0', b'0');
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `name`, `phone`, `address`, `isadmin`, `isvalidate`) VALUES (39, '3', '3@3', '3', '3', '3', '3', b'0', b'0');
